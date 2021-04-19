ace76074ac766231355ec07e20eedff19f356353
movichksu <pahomovichk@gmail.com>
2021-04-19 23:54:12 +0300

    add TypeConverter to birthday-entity

--- app/src/main/java/com/pahomovichk/remindMeDate/entity/Birthday.kt
+++ app/src/main/java/com/pahomovichk/remindMeDate/entity/Birthday.kt
@@ -2,10 +2,12 @@ package com.pahomovichk.remindMeDate.entity
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import androidx.room.TypeConverters
 import java.time.LocalDate
 import java.time.LocalTime
 
 @Entity
+@TypeConverters(BirthdayConverter::class)
 data class Birthday (
         @PrimaryKey(autoGenerate = true)
         val id: Long,
